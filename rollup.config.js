17754f145cfa3b14f566b5bebc6569fae5e30597
Mervin <mengqing723@gmail.com>
2020-07-09 13:43:56 +0800

    :construction_worker: 更改构建打包流程

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,43 @@ const babel = require('rollup-plugin-babel')
 const resolve = require('rollup-plugin-node-resolve')
 const json = require('@rollup/plugin-json')
 const commonjs = require('rollup-plugin-commonjs')
-const pkg = require('./package.json')
+const { uglify } = require('rollup-plugin-uglify')
+const replace = require('rollup-plugin-replace')
+const { name, version, author, license } = require('./package.json')
+
+const isProd = process.env.NODE_ENV === 'production' ? true : false
+const filename = 'index' + (isProd ? '.min' : '')
+
+const banner = `/*!
+ * ${name} v${version}
+ * (c) ${(new Date).getFullYear()} ${author}
+ * Released under the ${license} License.
+ */`
 
 module.exports = {
   input: 'src/main.js',
   plugins: [
     resolve(),
-    babel(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
     commonjs(),
-    json()
+    json(),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    (isProd && uglify({
+      output: {
+        preamble: banner
+      }
+    }))
   ],
   output: {
-    file: `dist/index.js`,
+    file: `dist/${filename}.js`,
     format: 'umd',
     name: 'Compressor',
-    sourcemap: true
+    sourcemap: !isProd,
+    banner
   }
 }
